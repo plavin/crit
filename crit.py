cf8397c500a2843bdcf7fb5f1c36dd4826f75e51
Patrick L <fohx13@gmail.com>
2016-03-28 01:30:17 -0400

    added comments

--- crit.py
+++ crit.py
@@ -2,7 +2,7 @@ import sys
 import bgqshared
 import contextlib
 
-offset = (-2,0,2)
+offset = (-2,0,2) 
 
 class DummyFile(object):
     def write(self, x): pass
@@ -14,16 +14,18 @@ def nostdout():
     yield
     sys.stdout = save_stdout
 
-def path_len(path):
+#path_len will be a measure of how conclicted the path is
+#(using that third number in the conflict links tuple)
+def path_len(path, conflictlinks):
     len = 0
     return len
 
+#create path from pt1 to pt2
 def path(pt1, pt2):
     p = []
-    
-    
     return p
 
+#get nodes in a communicator and put in a list
 def readNodeSet(file):
   list = []
   with open(file) as f:
@@ -34,22 +36,24 @@ def readNodeSet(file):
 def main(comm1,comm2):
     conflict_dict = []
     with nostdout():
+        #get conflicted links
         conflict_dict = bgqshared.main(comm1,comm2,1)
 
     comm1_points = readNodeSet(comm1)
 
     max_len = 0
-    max_path = []    
+    max_path = []
+    #measure each point to point distance
     for x in comm1_points:
         for a in offset:
             for b in offset:
                 for c in offset:
                     for d in offset:
                         x_prime = (x[0]+a,x[1]+b,x[2]+c,x[3]+d,0)
-                        if (x_prime in comm):
+                        if (x_prime in comm1_points): #pseudo code
                             p = path(x, x_prime)
                             if path_len(p) > max_len:
-                                max_len = path_len(p)
+                                max_len = path_len(p, conflictlinks)
                                 max_path = p
                             
 
